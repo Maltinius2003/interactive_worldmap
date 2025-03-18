9f79d91b47c710ad1307d13fb138c21085e74af6
Malte Geißler <malte.geissler@freseniusmedicalcare.com>
2025-03-18 13:17:12 +0100

    test led driver

--- led_controller/src/main.cpp
+++ led_controller/src/main.cpp
@@ -1,18 +1,37 @@
 #include <Arduino.h>
 
-// put function declarations here:
-int myFunction(int, int);
+// Pins am ESP8266 (NodeMCU z.B.)
+#define DATA_PIN D5  // SDI
+#define CLOCK_PIN D6 // CLK
+#define LATCH_PIN D7 // LE
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  pinMode(DATA_PIN, OUTPUT);
+  pinMode(CLOCK_PIN, OUTPUT);
+  pinMode(LATCH_PIN, OUTPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  // Muster 1: 10101010 10101010
+  sendData(0b1010101010101010);
+  delay(500);
+  
+  // Muster 2: 01010101 01010101
+  sendData(0b0101010101010101);
+  delay(500);
 }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+// sendet 16 Bit an das Register
+void sendData(uint16_t data) {
+  digitalWrite(LATCH_PIN, LOW); // Latch deaktivieren
+
+  for (int i = 15; i >= 0; i--) {
+    digitalWrite(CLOCK_PIN, LOW); // Clock low
+    digitalWrite(DATA_PIN, (data >> i) & 0x01); // Bit setzen
+    digitalWrite(CLOCK_PIN, HIGH); // Bit übernehmen
+  }
+
+  digitalWrite(LATCH_PIN, HIGH); // Latch übernehmen
+  delayMicroseconds(10);
+  digitalWrite(LATCH_PIN, LOW);  // Latch wieder deaktivieren
 }
