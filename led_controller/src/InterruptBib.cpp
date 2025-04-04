247e3fb89fa8f79ad57c90ec892fcdd17d0f550c
Maltinius <geissler.malte@posteo.de>
2025-04-04 12:29:38 +0200

    timer prescaler

--- led_controller/src/InterruptBib.cpp
+++ led_controller/src/InterruptBib.cpp
@@ -32,7 +32,7 @@ void setup() {
 
   timer1_isr_init();
   timer1_attachInterrupt(timer1ISR);
-  timer1_enable(TIM_DIV16, TIM_EDGE, TIM_LOOP);
+  timer1_enable(TIM_DIV16, TIM_EDGE, TIM_LOOP); //TIM_DIV16 = 80MHz/16 = 5MHz
   timer1_write(2350);
 }
 
